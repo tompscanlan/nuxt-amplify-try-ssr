9f2d1a7077c2d0e3d9725d2df87ccdb6f1fcb2d3
Tom Scanlan <tompscanlan@gmail.com>
2023-07-28 18:16:49 -0400

    initial, and comment out lint errs

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  customSyntax: 'postcss-html',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-prettier',
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    // "media-query-no-invalid": null,
+    // "selector-anb-no-unmatchable": null
+  },
+}
